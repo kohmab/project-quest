cd62232caccd871f63c68a507380e4ca20a65f8f
Ivan Pavlichenko <kohmab@gmail.com>
2024-06-06 22:04:03 +0300

    add null user filter

---
+++ src/main/java/com/javarush/pavlichenko/quest/controller/EmptyUserFiler.java
@@ -0,0 +1,23 @@
+package com.javarush.pavlichenko.quest.controller;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+import static java.util.Objects.isNull;
+
+public class EmptyUserFiler implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpSession session = httpServletRequest.getSession();
+        if (isNull(session.getAttribute("user"))){
+            HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+            httpServletResponse.sendRedirect("reset");
+        }
+        chain.doFilter(request,response);
+    }
+}
