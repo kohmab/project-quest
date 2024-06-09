e566ea8e383b377b0b9d8b6b9f7a46ef3ab21731
Ivan Pavlichenko <kohmab@gmail.com>
2024-06-09 16:04:28 +0300

    thread safe initalization

--- src/main/java/com/javarush/pavlichenko/quest/repository/QuestRepository.java
+++ src/main/java/com/javarush/pavlichenko/quest/repository/QuestRepository.java
@@ -4,38 +4,49 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javarush.pavlichenko.quest.entity.QuestNode;
-import lombok.Getter;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 
 public class QuestRepository {
     private static final String QUEST_FILE = "json/quest-tree.json"; //TODO move
 
-    @Getter
-    private static QuestRepository instance;
+    private static volatile QuestRepository instance;
 
     private Map<String, QuestNode> questNodes;
 
     private QuestRepository() {
     }
 
-    // TODO make thread safe
+    public static QuestRepository getInstance() {
+        try {
+            init();
+        } catch (IOException exception){
+            throw new RuntimeException(exception);
+        }
+
+        return instance;
+    }
+
     public static void init() throws IOException {
         if (nonNull(instance))
             return;
 
         ObjectMapper mapper = new ObjectMapper(new JsonFactory());
-        instance = new QuestRepository();
 
-        try (InputStream ioStream = QuestRepository.class.getClassLoader().getResourceAsStream(QUEST_FILE)) {
-            instance.questNodes = mapper.readValue(ioStream, new TypeReference<Map<String,QuestNode>>() {
-            });
+        synchronized (QuestRepository.class){
+            if (isNull(instance)){
+                QuestRepository localInstance = new QuestRepository();
+                try (InputStream ioStream = QuestRepository.class.getClassLoader().getResourceAsStream(QUEST_FILE)) {
+                    localInstance.questNodes = mapper.readValue(ioStream, new TypeReference<Map<String,QuestNode>>() {
+                    });
+                }
+                instance = localInstance;
+            }
         }
 
     }
